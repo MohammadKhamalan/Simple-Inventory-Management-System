d0bb70062a0c96948879dd51db73556e8ea57888
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-02-17 18:02:04 +0200

    Add Product feature with name, price, and quantity fields

---
+++ Inventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple_Inventory_Management_System
+{
+    class Inventory
+    {
+        public List<Product> products = new List<Product>();
+        public void Add_product(Product product)
+        {
+            products.Add(product);
+            Console.WriteLine($"product {product.Get_Name()} added successfully");
+        }
+
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple_Inventory_Management_System
+{
+    class Product
+    {
+        private String Name { get; set; }
+        private decimal Price { get; set; }
+        private int Quantity { get; set; }
+
+        public Product(String name, decimal price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+        public string Get_Name()
+        {
+            return Name; 
+        }
+       
+        public override string ToString()
+        {
+            return $"Product: {Name}, Price:{Price}, Quantuty:{Quantity}";
+        }
+    }
+}
