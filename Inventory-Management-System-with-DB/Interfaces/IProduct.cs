dfafeeb6185b35566df4d1d07d7b3d7aedbe9a8a
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-04-26 04:30:56 +0300

    Initial commit

---
+++ Inventory-Management-System-with-DB/Interfaces/IProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inventory_Management_System.Interfaces
+{
+    public interface IProduct
+    {
+        string Name { get; set; }
+        decimal Price { get; set; }
+        int Quantity { get; set; }
+        string ToString();
+    }
+}
